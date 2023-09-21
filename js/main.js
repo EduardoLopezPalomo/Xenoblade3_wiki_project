e9b76d35f36180645542d42ce157df21e2a69857
Eduardo <e.lpalomo@alumnos.upm.es>
2023-09-21 13:06:42 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,61 @@ const menuNav = document.querySelector(".menu-nav");
 const menubranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
+
+//set initial state of menu
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+//music
+
+let offseer = new Audio("./music/Off-seer.mp3");
+let LifeSentOn = new Audio("./music/A-life-sent-on.mp3");
+let WeightOfLife = new Audio("./music/The-weight-of-life.mp3");
+
+const playoffseer = document.querySelector(".play-off-seer");
+const playAlifesenton = document.querySelector(".life-sent-on");
+const playweightlife = document.querySelector(".weigth-of-life");
+
+playoffseer.addEventListener("click",offseermusic)
+playAlifesenton.addEventListener("click",LifeSentOnmusic)
+playweightlife.addEventListener("click",WeightOfLifemusic)
+function offseermusic(){
+    offseer.play();
+    LifeSentOn.pause();
+    WeightOfLife.pause();
+}
+function LifeSentOnmusic(){
+    LifeSentOn.play();
+    offseer.pause();
+    WeightOfLife.pause();
+}
+function WeightOfLifemusic(){
+    WeightOfLife.play();
+    LifeSentOn.pause();
+    offseer.pause();
+}
+
+function toggleMenu(){
+    if(!showMenu){
+        menuBtn.classList.add("close");
+        menu.classList.add("show");
+        menuNav.classList.add("show");
+        menubranding.classList.add("show");
+        navItems.forEach(item => item.classList.add("show"));
+
+        showMenu =true;
+    }
+    else{
+        menuBtn.classList.remove("close");
+        menu.classList.remove("show");
+        menuNav.classList.remove("show");
+        menubranding.classList.remove("show");
+        navItems.forEach(item => item.classList.remove("show"));
+
+        showMenu =false;
+    }
+}
+
 //select img of characters
 const imgNoah = document.querySelector(".bio-image-Noah");
 const imgMio = document.querySelector(".bio-image-Mio");
@@ -35,41 +90,6 @@ const JBIO = document.querySelector(".bio-J");
 const XBIO = document.querySelector(".bio-X");
 const YBIO = document.querySelector(".bio-Y");
 
-//set initial state of menu
-let showMenu = false;
-menuBtn.addEventListener("click", toggleMenu);
-
-//music
-
-let offseer = new Audio("./music/Off-seer.mp3");
-let LifeSentOn = new Audio("./music/A-life-sent-on.mp3");
-let WeightOfLife = new Audio("./music/The-weight-of-life.mp3");
-
-const playoffseer = document.querySelector(".play-off-seer");
-const playAlifesenton = document.querySelector(".life-sent-on");
-const playweightlife = document.querySelector(".weigth-of-life");
-
-playoffseer.addEventListener("click",()=>{
-    offseer.play();
-    LifeSentOn.pause();
-    WeightOfLife.pause();
-})
-playAlifesenton.addEventListener("click",()=>{
-    LifeSentOn.play();
-    offseer.pause();
-    WeightOfLife.pause();
-})
-playweightlife.addEventListener("click",()=>{
-    WeightOfLife.play();
-    LifeSentOn.pause();
-    offseer.pause();
-})
-
-
-
-
-
-
 //function
 imgNoah.addEventListener("click",BioCharacterNoah)
 imgMio.addEventListener("click",BioCharacterMio)
@@ -84,27 +104,6 @@ imgM.addEventListener("click",BioCharacterM)
 imgJ.addEventListener("click",BioCharacterJ)
 imgX.addEventListener("click",BioCharacterX)
 imgY.addEventListener("click",BioCharacterY)
-
-function toggleMenu(){
-    if(!showMenu){
-        menuBtn.classList.add("close");
-        menu.classList.add("show");
-        menuNav.classList.add("show");
-        menubranding.classList.add("show");
-        navItems.forEach(item => item.classList.add("show"));
-
-        showMenu =true;
-    }
-    else{
-        menuBtn.classList.remove("close");
-        menu.classList.remove("show");
-        menuNav.classList.remove("show");
-        menubranding.classList.remove("show");
-        navItems.forEach(item => item.classList.remove("show"));
-
-        showMenu =false;
-    }
-}
 
 // heroes
 function BioCharacterNoah(){
