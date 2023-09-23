51296c1b6cb13d08e4d6549dbbfb8814643597ae
Eduardo <e.lpalomo@alumnos.upm.es>
2023-09-23 17:00:22 +0300

    more music and characters mobile

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,31 +12,73 @@ menuBtn.addEventListener("click", toggleMenu);
 
 //music
 
-let offseer = new Audio("./music/Off-seer.mp3");
+let lifeOverflowing = new Audio("./music/A-Life-Overflowing.mp3");
 let LifeSentOn = new Audio("./music/A-life-sent-on.mp3");
 let WeightOfLife = new Audio("./music/The-weight-of-life.mp3");
+let ErythiaSea = new Audio("./music/Erythia-Sea-Night.mp3");
+let Bereaved = new Audio("./music/The-Bereaved-and-Those-Left-Behind.mp3");
+let KnowNames = new Audio("./music/You-Will-Know-Our-Names.mp3");
 
-const playoffseer = document.querySelector(".play-off-seer");
+const playlifeOverflowing = document.querySelector(".life-Overflowing");
 const playAlifesenton = document.querySelector(".life-sent-on");
 const playweightlife = document.querySelector(".weigth-of-life");
+const playErythiaSea = document.querySelector(".Erythia-sea");
+const playBereaved = document.querySelector(".bereaved-behind");
+const playKnownames = document.querySelector(".know-names");
 
-playoffseer.addEventListener("click",offseermusic)
+playlifeOverflowing.addEventListener("click",lifeOverflowingmusic)
 playAlifesenton.addEventListener("click",LifeSentOnmusic)
 playweightlife.addEventListener("click",WeightOfLifemusic)
-function offseermusic(){
-    offseer.play();
+playErythiaSea.addEventListener("click",ErythiaSeamusic)
+playBereaved.addEventListener("click",Bereavedmusic)
+playKnownames.addEventListener("click",knowNamesmusic)
+function lifeOverflowingmusic(){
+    lifeOverflowing.play();
     LifeSentOn.pause();
     WeightOfLife.pause();
+    ErythiaSea.pause();
+    Bereaved.pause();
+    KnowNames.pause();
 }
 function LifeSentOnmusic(){
     LifeSentOn.play();
-    offseer.pause();
+    lifeOverflowing.pause();
     WeightOfLife.pause();
+    ErythiaSea.pause();
+    Bereaved.pause();
+    KnowNames.pause();
 }
 function WeightOfLifemusic(){
     WeightOfLife.play();
     LifeSentOn.pause();
-    offseer.pause();
+    lifeOverflowing.pause();
+    ErythiaSea.pause();
+    Bereaved.pause();
+    KnowNames.pause();
+}
+function ErythiaSeamusic(){
+    lifeOverflowing.pause();
+    LifeSentOn.pause();
+    WeightOfLife.pause();
+    ErythiaSea.play();
+    Bereaved.pause();
+    KnowNames.pause();
+}
+function Bereavedmusic(){
+    lifeOverflowing.pause();
+    LifeSentOn.pause();
+    WeightOfLife.pause();
+    ErythiaSea.pause();
+    Bereaved.play();
+    KnowNames.pause();
+}
+function knowNamesmusic(){
+    lifeOverflowing.pause();
+    LifeSentOn.pause();
+    WeightOfLife.pause();
+    ErythiaSea.pause();
+    Bereaved.pause();
+    KnowNames.play();
 }
 
 function toggleMenu(){
